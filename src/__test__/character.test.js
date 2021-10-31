6266453c72a109bc369a222467b72bd54f341561
Nikita Shalimov <nikitashalimov@mail.ru>
2021-10-31 12:50:35 +0300

    тесты не проходят

---
+++ src/__test__/character.test.js
@@ -0,0 +1,48 @@
+import Character from '../character';
+
+test('проверка по длине имени', () => {
+  const unit1 = () => new Character('1', 'Boweman');
+  expect(unit1).toThrow("the character's name must contain letters from 2 to 10 characters");
+});
+
+test('проверка по выбору класса', () => {
+  const unit1 = () => new Character('unit', 'luchnik');
+  expect(unit1).toThrow('Please select one of the available classes: Boweman, Swordsman, Magician, Daemon, Undead, Zombie');
+});
+
+test('проверка основного персонажа', () => {
+  const unit = new Character('лучник', 'Boweman');
+  const unit2 = {
+    name: 'лучник',
+    type: 'Boweman',
+    health: 100,
+    level: 1,
+  };
+  expect(unit).toEqual(unit2);
+});
+
+test('проверка повышение уровня персонажем', () => {
+  const unit = new Character('робингуд', 'Boweman');
+  const unit3 = {
+    name: 'робингуд',
+    type: 'Boweman',
+    health: 100,
+    level: 2,
+    attack: 30,
+    defence: 30,
+  };
+  expect(unit.levelUp()).toBe(unit3);
+});
+
+test('проверка получения урона персонажем', () => {
+  const unit = new Character('стрелок', 'Boweman');
+  const unit4 = {
+    name: 'стрелок',
+    type: 'Boweman',
+    health: 92.5,
+    level: 1,
+    attack: 25,
+    defence: 25,
+  };
+  expect(unit.damage(10)).toBe(unit4);
+});
---
+++ src/boweman.js
@@ -0,0 +1,11 @@
+import Character from './character';
+
+export default class Boweman extends Character {
+  constructor(name, type = 'Boweman') {
+    super(name, type);
+    super.levelUp();
+    super.damage();
+    this.attack = 25;
+    this.defence = 25;
+  }
+}
